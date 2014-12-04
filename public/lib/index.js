0b19ed46d8466f3b0a214d01792cff9ed4b051d4
Essam Al Joubori <essam.joubori@gmail.com>
2014-12-04 10:48:26 -0500

    Added mentor page

--- public/lib/index.js
+++ public/lib/index.js
@@ -34,3 +34,59 @@ app.controller('GitHubCtrl', function ($scope, $http) {
   };
 });
 
+//dummy data for now. To be exctracted from db later
+  var student1 = {
+    name: "Essam",
+    subject: "Angular"
+  };
+  var student2 = {
+    name: "Victor",
+    subject: "D3"
+  };
+  var student3 = {
+    name: "Mike",
+    subject: "Backbone"
+  };
+
+  var students = [student1, student2, student3];
+
+
+app.controller('MentorCtrl', function ($scope, $http) {
+  $scope.getGitInfo = function () {
+    $scope.students = students;  //dummy data was added <<<<<<
+    $scope.userNotFound = false;
+    $scope.loaded = false;
+    $http.get("https://api.github.com/users/" + $scope.username)
+     .success(function (data) {
+        if ($scope.username !== undefined) {
+          $('#studentsTable').show();
+          $('#totalReputation').show();
+        }
+        
+        if (data.name === "") data.name = data.login;
+        $scope.user = data;
+        console.log(data);
+        $scope.loaded = true;
+     })
+     .error(function () {
+        $scope.userNotFound = true;
+     });
+    $http.get("https://api.github.com/users/" + $scope.username + "/repos").success(function (data) {
+      $scope.repos = data;
+      $scope.reposFound = data.length > 0;
+    });
+    var counter = 0;
+    $scope.up = function() {
+      counter++;
+      $('#reputation').html(counter);
+    };
+    $scope.down = function() {
+      if (counter >= 1) {
+        counter--;
+        $('#reputation').html(counter);
+      }    
+    }
+  };
+
+});
+
--- server.js
+++ server.js
@@ -3,9 +3,29 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
 
+
+//*****routes*****//
+// var routes = require('./routes/index');
+// var mentorRoute = require('./routes/mentor');
+
+// app.use('/', studentRoute);
+// app.use('/mentor', mentorRoute);
+// app.engine('html');
+
+
 app.use('/', express.static(path.join(__dirname, 'public')));
+app.set('views', __dirname + 'public');
+
+// app.set('view engine', html)
+// app.set('view engine', 'html');
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+
+// app.get('/', function(req, res, next){
+//     res.render('./mentor.html');
+// });
+
 
 app.listen(3000);
 
