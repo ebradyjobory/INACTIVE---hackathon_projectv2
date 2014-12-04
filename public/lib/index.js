f6a9051cbce3563ccf159752e9993555e25603b0
Victor Leung <victorleungtw@gmail.com>
2014-12-04 21:38:05 +0800

    refractor index page to use angular

--- public/lib/index.js
+++ public/lib/index.js
@@ -1,78 +1,23 @@
-var app = {};
+var app = angular.module('App', []);
 
-app.init = function(){
-  this.students = [];
-  this.studentsRepo = [];
-};
-
-app.fetchUser = function(username){
-  $.ajax({
-    url: 'https://api.github.com/users/' + username,
-    type: 'GET',
-    contentType: 'application/json',
-    success: function(data){
-      app.students.push(data);
-      app.addStudent();
-    },
-    error: function(data){
-      console.error('something went wrong');
-    },
-  });
-};
-
-app.fetchUserRepo = function(username){
-  $.ajax({
-    url: 'https://api.github.com/users/' + username + '/repos',
-    type: 'GET',
-    contentType: 'application/json',
-    success: function(data){
-      data.forEach(function (repo) {
-        app.studentsRepo.push(repo);
-      });
-      app.addStudentRepo();
-    },
-    error: function(data){
-      console.error('something went wrong');
-    },
-  });
-};
-
-app.addStudent = function(){
-  var student = app.students[0];
-
-  var name = '<h1>'+student.name+'</h1>';
-  var email = '<a href=mailto:'+student.email+'>'+student.email+'</a>';
-  var blog = '<p>'+student.blog+'</p>';
-  var location = '<p>'+student.location+'</p>';
-  var url = '<a href='+ student.url +'>'+student.url+'</p>';
-
-  var followers = '<p> <strong>Followers:</strong> '+student.followers+'</p>';
-  var following = '<p> <strong>Following:</strong>'+student.following+'</p>';
-  var publicRepos = '<p> <strong> Repos:</strong>'+student.public_repos+'</p>';
-
-  var image = '<img src=' + student.avatar_url +'>';
-
-  $('#studentImage').append(image);
-  $('#studentInfo').append(name + email + blog + location + url);
-  $('#studentMetrics').append(followers + following + publicRepos);
-};
-
-app.addStudentRepo = function(){
-  // var studentRepo = app.studentRepo[0];
-
-  app.studentsRepo.forEach(function(repo){
-    var name = '<li><span class="label label-primary">' + repo.name + '</span></li>';
-    $('#studentRepos').append(name);
-  });
-};
-
-$(document).ready(function(){
-  app.init();
-
-  $('.search').on('click', function(e){
-    e.preventDefault();
-    var $username = $('#username').val();
-    app.fetchUser($username);
-    app.fetchUserRepo($username);
-  });
+app.controller('GitHubCtrl', function ($scope, $http) {
+  $scope.getGitInfo = function () {
+    $scope.userNotFound = false;
+    $scope.loaded = false;
+    $http.get("https://api.github.com/users/" + $scope.username)
+     .success(function (data) {
+        if (data.name === "") data.name = data.login;
+        $scope.user = data;
+        console.log(data);
+        $scope.loaded = true;
+     })
+     .error(function () {
+        $scope.userNotFound = true;
+     });
+    $http.get("https://api.github.com/users/" + $scope.username + "/repos").success(function (data) {
+      $scope.repos = data;
+      $scope.reposFound = data.length > 0;
+    });
+  };
 });
+
